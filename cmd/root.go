f55fa35340e65a5a9ed5b06339e817e403e535cf
Daniel Hess <dan9186@gmail.com>
2018-01-30 03:40:12 -0800

    making output optionally verbose

--- cmd/root.go
+++ cmd/root.go
@@ -40,7 +40,7 @@ var RootCmd = &cobra.Command{
 
 func Execute() {
 	if err := RootCmd.Execute(); err != nil {
-		fmt.Printf("Failed to execute: %v\n", err.Error())
+		printf("Failed to execute: %v\n", err.Error())
 		os.Exit(1)
 	}
 }
@@ -48,7 +48,7 @@ func Execute() {
 func probe(cmd *cobra.Command, args []string) {
 	pool, err := gocertifi.CACerts()
 	if err != nil {
-		fmt.Printf("Error: failed to create cert pool: %v\n", err.Error())
+		printf("Error: failed to create cert pool: %v\n", err.Error())
 	}
 
 	tlsConfig := &tls.Config{
@@ -67,10 +67,22 @@ func probe(cmd *cobra.Command, args []string) {
 
 	resp, err := client.Get(args[0])
 	if err != nil {
+		verbosef("error: http get: %v", err.Error())
 		os.Exit(1)
 	}
 
 	if resp.StatusCode != http.StatusOK {
+		verbosef("error: status %v", resp.StatusCode)
 		os.Exit(1)
+	}
+}
+
+func printf(f string, args ...interface{}) {
+	fmt.Println(fmt.Sprintf(f, args...))
+}
+
+func verbosef(f string, args ...interface{}) {
+	if verbose {
+		fmt.Println(fmt.Sprintf(f, args...))
 	}
 }
